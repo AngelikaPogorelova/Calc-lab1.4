29305df0908f4a1464228a52ff2975df886030ff
User <User@LAPTOP-Q7VE0N0C>
2026-04-08 22:39:26 +0400

    Добавьте файлы проекта.

---
+++ Calc/Controllers/CalcController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calc.Models;
+
+namespace Calc.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new CalcModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalcModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            switch (model.Operation)
+            {
+                case "+":
+                    model.Result = model.Operand1.Value + model.Operand2.Value;
+                    break;
+
+                case "-":
+                    model.Result = model.Operand1.Value - model.Operand2.Value;
+                    break;
+
+                case "*":
+                    model.Result = model.Operand1.Value * model.Operand2.Value;
+                    break;
+
+                case "/":
+                    if (model.Operand2 != 0)
+                        model.Result = (decimal)model.Operand1.Value / model.Operand2.Value;
+                    break;
+            }
+
+            string expr = $"{model.Operand1} {model.Operation} {model.Operand2} = {model.Result}";
+            Response.Cookies.Append("calc", expr);
+
+            ViewBag.CheckValue = 10;
+
+            return View(model);
+        }
+
+        public IActionResult Result()
+        {
+            string expression = Request.Cookies["calc"];
+
+            if (expression == null)
+                expression = "Нет данных";
+
+            // ЗАМЕНА "=" на " равно "
+            int index = expression.IndexOf("=");
+
+            if (index != -1)
+            {
+                expression = expression.Remove(index, 1);
+                expression = expression.Insert(index, " равно ");
+            }
+
+            ViewBag.Expression = expression;
+
+            return View();
+        }
+
+    }
+}
---
+++ Calc/Models/CalcModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Calc.Models
+{
+    public class CalcModel
+    {
+        [Required(ErrorMessage = "Введите первое число")]
+        public long? Operand1 { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Введите число от 1 до 1000")]
+        public long? Operand2 { get; set; }
+
+        public string? Operation { get; set; }
+
+        public decimal Result { get; set; }
+    }
+}
